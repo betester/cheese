47a5668986d2586a3872bff1a117ac88c6cf3e22
betester <ikramzulkarnaen02@gmail.com>
2024-05-08 21:51:58 +0700

    change back to 8

--- chess.c
+++ chess.c
@@ -8,7 +8,7 @@ void move_piece(struct Piece (*board)[8], struct Piece currentLoc,
 void render_board(struct Piece (*board)[8]) {
 
   for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < sizeof(board[i]) / sizeof(struct Piece); j++) {
+    for (int j = 0; j < 8; j++) {
       printf("%s", board[i][j].pieceUnicode);
     }
     printf("\n");
