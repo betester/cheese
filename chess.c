00dea59138ecb67686617d1b86118709a5ae0ffd
betester <ikramzulkarnaen02@gmail.com>
2024-05-09 12:35:52 +0700

    i included main.o by accident

--- chess.c
+++ chess.c
@@ -2,14 +2,47 @@
 #include "chess.h"
 #include <stdio.h>
 
-void move_piece(struct Piece (*board)[8], struct Piece currentLoc,
-                struct Piece nextLocation) {}
+// 1 indexed because why not ?
+struct PieceMovement char_movements[13];
+int total_piece_movement_set = 0;
 
-void render_board(struct Piece (*board)[8]) {
+void set_move_rule(struct PieceMovement piece_movement) {
 
+  int next_position = total_piece_movement_set + 1;
+
+  for (int i = 0; i <= total_piece_movement_set; i++) {
+
+    if (char_movements[i].piece == piece_movement.piece) {
+      printf("Already assign %s movement rule replacing the rule\n",
+             piece_movement.piece);
+      next_position = i;
+      break;
+    }
+  }
+
+  if (next_position >= 13) {
+    printf("Cannot set more than 8 rules for piece movement for now\n");
+    return;
+  }
+
+  printf("Setting up rule for piece %s\n", piece_movement.piece);
+  for (int i = 0; i < 8; i++) {
+    char_movements[next_position].allowed_movements[i] =
+        piece_movement.allowed_movements[i];
+  }
+
+  if (next_position > total_piece_movement_set) {
+    total_piece_movement_set = next_position;
+  }
+}
+
+void move_piece(char (*board)[8][8], struct Location curr_loc,
+                struct Location next_loc) {}
+
+void render_board(char (*board)[8][8]) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      printf("%s", board[i][j].pieceUnicode);
+      printf("%s", board[i][j]);
     }
     printf("\n");
   }
